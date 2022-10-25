fff3f0675de73486bc4212747d6c1c40bda87996
Elsti97 <elsti.shehati@hotmail.it>
2022-10-25 14:12:54 +0200

    Snack 1

---
+++ main.js
@@ -0,0 +1,15 @@
+// * JSnack 1
+// let number = parseInt(prompt("Inserisci numero"))
+// if(number % 2 == 0) {
+//   console.log(number + 1)
+//   risultato.innerHTML = `Il tuo numero è ${number+1}, prossima volta scegli direttamente un numero dispari dai`
+// } else if (number % 2 !== 0) {
+//   console.log(number)
+//   risultato.innerHTML = `Il tuo numero è ${number}, bravo hai scelto un numero dispari`
+// }
+
+
+
+// * JSnack 2
+let name []
+let surname []
